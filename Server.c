20352e8f1cfa71baddce808e34d67c6f215ffe3c
aliffali12 <aliffali28@gmail.com>
2019-03-25 13:16:46 +0800

    Updated version of Server

--- Server.c
+++ Server.c
@@ -1,365 +1,103 @@
-
-
-
-
-#include <unistd.h> 
-
-
-
+ 
 #include <stdio.h> 
-
-
-
 #include <sys/socket.h> 
-
-
-
 #include <stdlib.h> 
-
-
-
 #include <netinet/in.h> 
-
-
-
 #include <string.h> 
-
-
+#include <unistd.h>
 
 #define PORT 9002
 
 
 int main(int argc, char const *argv[]) 
-
-
-
-
-
-
-
 { 	
-
-
 
 	setbuf(stdout,NULL);
 
-
-
-	printf("Creating socket..");
-
-
-
-	int server_fd, new_socket, valread; 
-
-
-
-
-
-
+	int server_new, new_sock, vread; 
 
 	struct sockaddr_in address; 
 
-
-
-
-
-
-
 	int opt = 1; 
-
-
-
-
-
-
 
 	int addrlen = sizeof(address); 
 
+	char buffer [1024] ; 
 
-
-
-
-
-
-	char buffer[1024] = {0}; 
-
-
-
-
-
-
-
-	char *welcome = "Welcome and Hello"; 
-
-
-
-
-
-
-
-	
-
-
+	char welcome [256];
+ 
 
 	// Creating socket file descriptor 
 
 
-
-
-
-
-
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-
-
-
-
-
-
+	if ((server_new = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
 
 	{ 
 
-
-
-
-
-
-
 		perror("socket failed"); 
-
-
-
-
-
-
 
 		exit(EXIT_FAILURE); 
 
-
-
-
-
-
-
 	} 
 
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-	// Forcefully attaching socket to the port 8080 
-
-
-
-
-
-
-
-
-
-
+	// Forcefully attaching a socket to the port 9002 
 
 	address.sin_family = AF_INET; 
 
-
-
-
-
-
-
 	address.sin_addr.s_addr = INADDR_ANY; 
-
-
-
-
-
-
 
 	address.sin_port = htons(PORT); 
 
 
 
+        // Forcefully attaching socket to the port 9002 
 
 
-
-
-	
-
-
-
-
-
-
-
-    // Forcefully attaching socket to the port 8080 
-
-
-
-
-
-
-
-    if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0) 
-
-
+    if (bind(server_new, (struct sockaddr *)&address,sizeof(address))<0) 
 
     { 
 
-
-
-
-
-
-
-        perror("bind failed"); 
-
-
-
-
-
-
+        perror("binding failed"); 
 
         exit(EXIT_FAILURE); 
 
-
-
-
-
-
-
     } 
 
-
-
-    
-
-
-
-    
-
-
-
-    if (listen(server_fd, 3) < 0) 
-
-
-
-
-
-
+    if (listen(server_new, 3) < 0) 
 
     { 
 
+        perror("listening"); 
 
-
-
-
-
-
-        perror("listen"); 
-
-
-
-
-
-
-
-        exit(EXIT_FAILURE); 
-
-
-
-
-
-
+        exit(EXIT_FAILURE);
 
     } 
 
+	memset(buffer ,0, sizeof(buffer));
 
-
-
-
-
-
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+    if ((new_sock = accept(server_new, (struct sockaddr *)&address, 
 (socklen_t*)&addrlen))<0) 
 
 
-
-
-
-
-
     { 
 
-
-
-
-
-
-
-        perror("accept"); 
-
-
-
-
-
-
+        perror("accepted by Server"); 
 
         exit(EXIT_FAILURE); 
 
-
-
-
-
-
-
     } 
 
+    vread = read( new_sock , buffer, 1024); 
 
+    recv(new_sock, buffer, sizeof(buffer), 1);
+    printf("Message From Client: %s", buffer);
+    recv(new_sock, buffer, sizeof(buffer), 0);
+    printf("%s",buffer);
+    printf("\n");   
 
+    scanf("%[^\n]%*c", welcome);
 
-
-
-
-    valread = read( new_socket , buffer, 1024); 
-
-
-
-
-
-
-
-    printf("%s\n",buffer ); 
-
-
-
-
-
-
-
-    send(new_socket , welcome , strlen(welcome) , 0 ); 
-
-
-
-
-
-
-
+    send(new_sock , welcome , strlen(welcome) , 0 ); 
     return 0; 
-
-
-
-
-
-
 
 } 
