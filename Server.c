6d5280da90ea443b9f98c3712eb4be4d24c08189
aliffali12 <aliffali28@gmail.com>
2019-03-26 12:04:12 +0800

    New Coding for Server

--- Server.c
+++ Server.c
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
  
-=======
-
->>>>>>> d55bc79c50e14322b8edac38424a0089e1b286e2
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-<<<<<<< HEAD
 #include <unistd.h>
-=======
-#include <unistd.h> 
-
->>>>>>> d55bc79c50e14322b8edac38424a0089e1b286e2
 
 #define PORT 9002
 
+
 int main(int argc, char const *argv[]) 
 { 	
-<<<<<<< HEAD
 
 	setbuf(stdout,NULL);
 
@@ -70,59 +61,11 @@ int main(int argc, char const *argv[])
 
         exit(EXIT_FAILURE); 
 
-=======
-	setbuf(stdout,NULL);
-
-	printf("trying to make a new socket");
-	
-	int server_new, new_sock, vread; 
-	
-	struct sockaddr_in address; 
-	
-	int opt = 1; 
-	
-	int addrlen = sizeof(address); 
-
-	char buffer[1024] = {0}; 
-
-	char *welcome = "Welcome and Hello"; 
-
-	// Creating socket file descriptor 
-
-	if ((server_new = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-
-	{ 
-		perror("socket failed"); 
-
-		exit(EXIT_FAILURE); 
-		
-	} 
-
-	// Forcefully attaching socket to the port 9002
-
-	address.sin_family = AF_INET; 
-
-	address.sin_addr.s_addr = INADDR_ANY;
-	
-	address.sin_port = htons(PORT);
-
-   	 // Forcefully attaching socket to the port 9002
-
-    if (bind(server_new, (struct sockaddr *)&address,sizeof(address))<0) 
-    
-    { 
-	    
-        perror("binding failed"); 
-
-        exit(EXIT_FAILURE); 
-	    
->>>>>>> d55bc79c50e14322b8edac38424a0089e1b286e2
     } 
 
     if (listen(server_new, 3) < 0) 
 
     { 
-<<<<<<< HEAD
 
         perror("listening"); 
 
@@ -138,20 +81,6 @@ int main(int argc, char const *argv[])
 
     { 
 
-=======
-	    
-        perror("listening"); 
-	    
-        exit(EXIT_FAILURE); 
-
-    } 
-
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-(socklen_t*)&addrlen))<0) 
-
-    { 
-	    
->>>>>>> d55bc79c50e14322b8edac38424a0089e1b286e2
         perror("accepted by Server"); 
 
         exit(EXIT_FAILURE); 
@@ -160,7 +89,6 @@ int main(int argc, char const *argv[])
 
     vread = read( new_sock , buffer, 1024); 
 
-<<<<<<< HEAD
     recv(new_sock, buffer, sizeof(buffer), 1);
     printf("Message From Client: %s", buffer);
     recv(new_sock, buffer, sizeof(buffer), 0);
@@ -172,12 +100,4 @@ int main(int argc, char const *argv[])
     send(new_sock , welcome , strlen(welcome) , 0 ); 
     return 0; 
 
-=======
-    printf("%s\n",buffer ); 
-
-    send(new_sock , welcome , strlen(welcome) , 0 ); 
-
-    return 0; 
-	
->>>>>>> d55bc79c50e14322b8edac38424a0089e1b286e2
 } 
